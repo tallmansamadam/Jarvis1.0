c77a306b1beb2df1f4263c15dc45f2730bf5ab30
tallmansamadam <tallman.samadam@gmail.com>
2023-01-08 15:35:26 -0600

    Final V1 Commit

--- client/script.js
+++ client/script.js
@@ -75,15 +75,15 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
-    method: 'POST',
+  const response = await fetch("https://jarvis-v1-0.onrender.com", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: data.get('prompt')
-    })
-  })
+      prompt: data.get("prompt"),
+    }),
+  });
 
   clearInterval(loadInterval);
   messageDiv.innerHTML = " ";
